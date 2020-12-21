627d954f34adcddf392e071d93540f9f4ec17dfa
Henry <henrygabrielgm@gmail.com>
2020-12-21 12:32:59 -0400

    new files and basic math functions

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const add = (num1,num2) => {
+    return num1 + num2;
+}
+const subtract = (num1,num2) => {
+    return num1 - num2;
+}
+const multiply = (num1,num2) => {
+    return num1*num2;
+}
+const divide = (num1,num2) => {
+    if(num2 == 0) {return 'Division by zero is undefined'};
+}
