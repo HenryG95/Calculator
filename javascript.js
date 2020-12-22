b43b76c3515cee7c7a9f3fc27a309449d84828bb
Henry <henrygabrielgm@gmail.com>
2020-12-21 20:52:59 -0400

    finished white calculator design

--- javascript.js
+++ javascript.js
@@ -11,3 +11,7 @@ const divide = (num1,num2) => {
     if(num2 == 0) {return 'Division by zero is undefined'};
     return num1/num2;
 }
+const operate = (num1,num2,operator) => {
+    return operator(num1,num2);
+   
+}
