659aa5fcd273d90fb66e69dbffbaa0fe6ad7b761
Henry <henrygabrielgm@gmail.com>
2020-12-22 21:39:04 -0400

    updating functionsto lok like android calculator

--- javascript.js
+++ javascript.js
@@ -1,18 +1,59 @@
-const add = (num1,num2) => {
-    return num1 + num2;
+const add = (array) => {
+    var result = 0;
+    array.forEach(item => {
+        result = result + item;
+    })
+    return result;
 }
-const subtract = (num1,num2) => {
-    return num1 - num2;
+const subtract = (array) => {
+    var result = 0;
+    array.forEach(item => {
+        result = item - result;
+    })
+    return result * -1;
 }
-const multiply = (num1,num2) => {
-    return num1 * num2;
+const multiply = (array) => {
+    var result = 1;
+    array.forEach(item => {
+        result = result * item;
+    })
+    return result;
 }
-const divide = (num1,num2) => {
-    if(num2 == 0) {return 'Division by zero is undefined'};
-    return num1/num2;
+const divide = (array) => {
+    var result = array[0];
+    for(var i =1;i<array.length;i++) {
+        result = result/array[i];
+    }
+    return result;
 }
-const compute = (num1,num2,operator) => {
-    return operator(num1,num2);
+const compute = (array,operator) => {
+    return operator(array);
+}
+
+const parse = (array) => {
+    var result = array.map(item => {
+        return parseFloat(item);
+    })
+    return result;
+}
+
+const endCompute = (content) => {
+    var numbers = content.split(check(content));
+    if(numbers.includes('')) {
+        numbers.splice(numbers.length -1,1);
+    }
+    parse(numbers);
+    if(numbers.length == 1) {
+        secondScreen.textContent = '';
+    } else {
+    var symbols = check(content);
+    console.log(numbers);
+    console.log(parse(numbers));
+    console.log(symbols);
+    var result  = compute(parse(numbers),checkSign(symbols));
+    secondScreen.textContent = result;
+    display.appendChild(secondScreen);
+    }
 }
 
 const checkSign = (sign) => {
@@ -39,6 +80,17 @@ const check = (result) => {
     if(result.includes('%')) {return '%'}
 }
 
+const verify = (sign) => {
+    if(sign.includes('+') || sign.includes('-') || sign.includes('*') || sign.includes('%')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let secondScreen = document.createElement('div');
+secondScreen.setAttribute('id','secondScreen');
+let display = document.querySelector('#display');
 let screen = document.querySelector('#screen');
 const pad = document.querySelectorAll('.pad');
 const operators = document.querySelectorAll('.operator');
@@ -48,6 +100,10 @@ let result = document.querySelector('.result');
 pad.forEach(item => {
     item.addEventListener('click', () => {
         screen.textContent = screen.textContent + item.getAttribute('id');
+        if(verify(screen.textContent)) {
+            endCompute(screen.textContent);
+            
+        }
     });
 })
 
@@ -57,30 +113,23 @@ operators.forEach(item => {
     });
 })
 
+
 clear.addEventListener('click', () => {
-    var content = screen.textContent;
-    var result = content.substring(0,content.length -1)
-    console.log(result);
-    screen.textContent = result;
-})
+        var content = screen.textContent;
+        var result = content.substring(0,content.length -1);
+        screen.textContent = result;
+        endCompute(result);
+});
 
 result.addEventListener('click',() => {
     var content = screen.textContent;
-    var numbers = content.split(check(content));
-    console.log(numbers);
-    var symbols = check(content);
-    console.log(symbols);
-    console.log(typeof parseInt(numbers[0]));
-    var result  = compute(parseFloat(numbers[0]),parseFloat(numbers[1]),checkSign(symbols));
-    screen.textContent = result;
+    endCompute(content);
+    screen.textContent = secondScreen.textContent;
+    secondScreen.textContent = '';
     if(screen.textContent != '') {
         clear.addEventListener('click', () => {
             screen.textContent = '';
         })
     }
         
-})
-
-clear.addEventListener('mouseup', () => {
-    screen.textContent = '';
 })
